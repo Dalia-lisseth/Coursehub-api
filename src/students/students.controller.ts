1727cf86f85f57694be0add316f326ba7678eca0
Dalia-lisseth <e1315962629@live.uleam.edu.ec>
2026-09-15 17:31:06 -0500

    arreglos

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { StudentsService } from './students.service.js';
 import { CreateStudentDto } from './dto/create-student.dto/create-student.dto.js';
 
@@ -11,8 +11,23 @@ export class StudentsController {
     return this.studentsService.findAll();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.studentsService.findOne(Number(id));
+  }
+
   @Post()
   create(@Body() createStudentDto: CreateStudentDto) {
     return this.studentsService.create(createStudentDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateStudentDto: Partial<CreateStudentDto>) {
+    return this.studentsService.update(Number(id), updateStudentDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.studentsService.delete(Number(id));
   }
 }
--- src/students/students.service.ts
+++ src/students/students.service.ts
@@ -1,22 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateStudentDto } from './dto/create-student.dto/create-student.dto.js';
 
 @Injectable()
 export class StudentsService {
-  private students: CreateStudentDto[] = [];
+  private students: Array<CreateStudentDto & { id: number }> = [];
 
   findAll() {
     return this.students;
   }
 
+  findOne(id: number) {
+    const student = this.students.find((item) => item.id === id);
+
+    if (!student) {
+      throw new NotFoundException(`Student with id ${id} not found`);
+    }
+
+    return student;
+  }
+
   create(createStudentDto: CreateStudentDto) {
     const student = {
-      id: Date.now().toString(),
+      id: this.students.length + 1,
       ...createStudentDto,
     };
 
     this.students.push(student);
 
     return student;
+  }
+
+  update(id: number, updateStudentDto: Partial<CreateStudentDto>) {
+    const studentIndex = this.students.findIndex((item) => item.id === id);
+
+    if (studentIndex === -1) {
+      throw new NotFoundException(`Student with id ${id} not found`);
+    }
+
+    this.students[studentIndex] = {
+      ...this.students[studentIndex],
+      ...updateStudentDto,
+    };
+
+    return this.students[studentIndex];
+  }
+
+  delete(id: number) {
+    const studentIndex = this.students.findIndex((item) => item.id === id);
+
+    if (studentIndex === -1) {
+      throw new NotFoundException(`Student with id ${id} not found`);
+    }
+
+    const [deletedStudent] = this.students.splice(studentIndex, 1);
+
+    return deletedStudent;
   }
 }
